3a79727a01a3372326c54b2e53fd1ff973365190
beylulari <71577441+beylulari@users.noreply.github.com>
2023-12-04 16:13:31 -0700

    update WelcomeActivity.java

--- src/app/src/main/java/com/example/sweethome/WelcomeActivity.java
+++ src/app/src/main/java/com/example/sweethome/WelcomeActivity.java
@@ -1,5 +1,7 @@
 package com.example.sweethome;
 
+/* necessary imports */
+
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -12,7 +14,19 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.List;
+
+/**
+ * @class WelcomeActivity
+ *
+ * <p>This class serves as an entry point to the SweetHome application.
+ * It checks whether a user is currently signed in and takes them to the
+ * main screen if so, otherwise it takes them to the login page. </p>
+ *
+ * @date <p>December 1, 2023</p>
+ */
 public class WelcomeActivity extends AppCompatActivity {
+    /* attributes of this class */
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference usersRef = db.collection("users");
     private FirebaseAuth userAuth = FirebaseAuth.getInstance();
@@ -20,7 +34,6 @@ public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_welcome);
 
         // Check if user is signed in (non-null)
         FirebaseUser currentUser = userAuth.getCurrentUser();
@@ -33,27 +46,38 @@ public class WelcomeActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             QuerySnapshot q = task.getResult();
                             if (q != null) {
-                                DocumentSnapshot doc = q.getDocuments().get(0);
-                                if (doc != null && doc.exists()) {
+                                List docs = q.getDocuments();
+                                if (docs.size() != 0 && q.getDocuments().get(0) != null && q.getDocuments().get(0).exists()) {
+                                    // user exists, take them to their main page
+                                    DocumentSnapshot doc = q.getDocuments().get(0);
                                     User user = doc.toObject(User.class);
-                                    assert user != null;
-                                    String username = user.getUsername();
-                                    Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
-                                    intent.putExtra("USERNAME", username); //send username to main activity
-                                    startActivity(intent);
-                                    finish(); // Close the WelcomeActivity once the process is complete
+                                    if (user != null) {
+                                        String username = user.getUsername();
+                                        Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                                        intent.putExtra("USERNAME", username); //send username to main activity
+                                        startActivity(intent);
+                                        finish(); // Close the WelcomeActivity once the process is complete
+                                    } else { // if we cannot find the user for whatever reason also go to login activity
+                                        goToLogin();
+                                    }
                                 } else { // if we cannot find the user for whatever reason also go to login activity
-                                    Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
-                                    startActivity(intent);
-                                    finish(); // Close the WelcomeActivity
+                                    goToLogin();
                                 }
+                            } else { // if we cannot find the user for whatever reason also go to login activity
+                                goToLogin();
                             }
+                        } else { // if we cannot find the user for whatever reason also go to login activity
+                            goToLogin();
                         }
                     });
         } else { // otherwise take them to the login activity
-            Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
-            startActivity(intent);
-            finish(); // Close the WelcomeActivity
+            goToLogin();
         }
+    }
+
+    private void goToLogin() {
+        Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish(); // Close the WelcomeActivity
     }
 }
